54c1703fa9aec1de6e76544894c4064d66e94ebb
fagneraureliano <fagner2aureliano@gmail.com>
2023-06-08 09:40:43 -0300

    feat:(hero component): impl hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
 import { HEROES } from '../heroes.mock';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent {
-  hero: Hero = { id: 1, name: 'Wolverine' };
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
   selectedHero!: Hero;
-
+  constructor(private heroService: HeroService) {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   onSelect(hero: Hero) {
-this.selectedHero = hero;
+    this.selectedHero = hero;
   }
 }
