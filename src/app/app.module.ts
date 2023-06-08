05fdfcd557f0b9ee402b4bedc1a1622a6ed6ea99
fagneraureliano <fagner2aureliano@gmail.com>
2023-06-08 15:18:26 -0300

    feat:(MEssageComponent): impl app router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
 import { HeroDetailsComponent } from './heroe-details/hero-details.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessagesComponent } from './messages/messages.component';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MessagesComponent } from './messages/messages.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -19,11 +24,15 @@ import { MessagesComponent } from './messages/messages.component';
     MessagesComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
     MatExpansionModule,
   ],
   providers: [],
