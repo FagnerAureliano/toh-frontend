3d71361fdbdaff9890f9e740705df142f0e115f1
fagneraureliano <fagner2aureliano@gmail.com>
2023-06-13 18:53:58 -0300

    feat(search): impl search hero c input

--- src/app/shared/components/hero-search/hero-search.component.ts
+++ src/app/shared/components/hero-search/hero-search.component.ts
@@ -1,6 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
-import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import {
+  Observable,
+  Subject,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+} from 'rxjs';
 import { Hero } from 'src/app/core/models/hero.model';
 import { HeroService } from 'src/app/core/services/hero.service';
 
@@ -13,7 +19,6 @@ export class HeroSearchComponent implements OnInit {
   heroes$!: Observable<Hero[]>;
   @Input() label = '';
   @Output() private selected = new EventEmitter<Hero>();
-
   private searchTerms = new Subject<string>();
 
   constructor(private heroService: HeroService) {}
@@ -31,8 +36,6 @@ export class HeroSearchComponent implements OnInit {
   }
 
   onSelected(selectedItem: MatAutocompleteSelectedEvent) {
-    console.log(selectedItem.option.value);
-
     this.searchTerms.next('');
     const hero: Hero = selectedItem.option.value;
     this.selected.emit(hero);
